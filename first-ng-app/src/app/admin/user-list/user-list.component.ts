86e2346f04f27e1bec3eac4042bdcd6d80accf61
Hao-xy <2221973128@qq.com>
2022-05-21 19:55:20 +0800

    add note for gradle

--- first-ng-app/src/app/admin/user-list/user-list.component.ts
+++ first-ng-app/src/app/admin/user-list/user-list.component.ts
@@ -21,10 +21,6 @@ export class UserListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this._userApiService.count().subscribe((response) => {
-    //   const count = response.count;
-    //   this.msgs = [{summary: `${count} users`, severity: "info"}];
-    // })
     this._userApiService.count().subscribe(({count}) => {
       this.msgs = [{summary: `${count} users`, severity: "info"}];
     })
@@ -45,13 +41,19 @@ export class UserListComponent implements OnInit {
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
         //confirm action
-        this._userApiService.delete(id).subscribe(() => {
-          // reload user list
-          this.ngOnInit();
+        this._userApiService.delete(id).subscribe({
+          next: () => {
+            // reload user list
+            this.ngOnInit();
+          },
+          error: (message) => {
+            alert(JSON.stringify(message));
+          }
         })
       },
       reject: () => {
         //reject action
+        console.log("master branch")
       }
     });
   }
